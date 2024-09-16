f877a3b558e409143c7af9215f68310902c5c41e
Riyadh <zmriyadh@gmail.com>
2024-09-16 17:18:24 +0500

    first commit

---
+++ datatype.js
@@ -0,0 +1,16 @@
+var totalamount = 1000;
+var alu=100;
+var tomato=80;
+var peyaj=120;
+var potol=60
+
+var alutomato = alu+tomato
+var peyajpotol = peyaj+potol
+var total = alutomato+peyajpotol
+var reminder = totalamount-total
+
+console.log(alutomato)
+console.log(peyajpotol)
+console.log(total)
+console.log(reminder)
+
